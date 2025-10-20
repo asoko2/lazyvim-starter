e127b25556992d775b5d8493eed22365578cfea9
Anang Hariyanto N <sokoanang@gmail.com>
2025-10-20 17:15:24 +0700

    update options and keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Jump down while keeping the cursor on the center of screen" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Jump up while keeping the cursor on the center of screen" })
--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -40,7 +40,7 @@ return {
     end,
   },
 
-  -- change some telescope options and a keymap to browse plugin files
+  -- change some telescope (options) and a keymap to browse plugin files
   {
     "nvim-telescope/telescope.nvim",
     keys = {
